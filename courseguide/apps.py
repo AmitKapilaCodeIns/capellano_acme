c8b48d44ee6f14c6c188c61cd8c87d8ebda86139
AmitKapilaCodeIns <amitkapila@hotmail.com>
2025-08-07 15:39:08 +0100

    modify your app config class to connect the signals ensuring signal handlers are imported early

--- courseguide/apps.py
+++ courseguide/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CourseguideConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'courseguide'
+
+    def ready(self):
+        # Import signals or other initialization code here if needed
+        import courseguide.signals  # Ensure signals are imported
