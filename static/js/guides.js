807c702c896235f305d2d84ccf791e5e49d4809e
AmitKapilaCodeIns <amitkapila@hotmail.com>
2025-08-08 01:09:03 +0100

    get content from hidden span elements then set form fields with the retrieved content

--- static/js/guides.js
+++ static/js/guides.js
@@ -4,30 +4,49 @@ const holeForm = document.getElementById("holeForm");
 const submitButton = document.getElementById("submitButton");
 
 // Form fields for editing hole guides
-const holeText = document.getElementById("id_guide");
-console.log(holeText);
-console.log("Hello from guides.js");
+const formNumber = document.getElementById("id_hole_number");
+const formName = document.getElementById("id_name");
+const formPar = document.getElementById("id_par");
+const formYardage = document.getElementById("id_yardage");
+const formSI = document.getElementById("id_stroke_index");
+const formText = document.getElementById("id_guide");
+
 
 /**
-* Initializes edit functionality for the provided edit buttons.
-* 
-* For each button in the `editButtons` collection:
-* - Retrieves the associated guide's ID upon click.
-* - Fetches the content of the corresponding guide.
-* - Populates the `guideText` input/textarea with the guide's content for editing.
-* - Updates the submit button's text to "Update".
-* - Sets the form's action attribute to the `edit/{guide_id}` endpoint.
-*/
+ * Initializes edit functionality for the provided edit buttons.
+ * 
+ * For each button in the `editButtons` collection:
+ * - Retrieves the associated guide's ID upon click.
+ * - Fetches the content of the corresponding guide.
+ * - Populates the `guideText` input/textarea with the guide's content for editing.
+ * - Updates the submit button's text to "Update".
+ * - Sets the form's action attribute to the `edit/{guide_id}` endpoint.
+ */
 for (let button of editButtons) {
   button.addEventListener("click", (e) => {
     let holeId = e.target.getAttribute("hole_id");
 
+    // Get content from hidden span elements
+    let holeNum = document.getElementById(`hole-num-${holeId}`).innerText;
+    let holeName = document.getElementById(`hole-name-${holeId}`).innerText;
+    let par = document.getElementById(`hole-par-${holeId}`).innerText;
+    let yardage = document.getElementById(`hole-yardage-${holeId}`).innerText;
+    let strokeIndex = document.getElementById(`hole-si-${holeId}`).innerText;
+    
+
+    // Set form fields with the retrieved content
+    formNumber.value = holeNum;
+    formName.value = holeName;
+    formPar.value = par;
+    formYardage.value = yardage;
+    formSI.value = strokeIndex;
+
     let holeContent = document.getElementById(`hole${holeId}`).innerText;
 
-    holeText.value = holeContent;
+    formText.value = holeContent;
 
     // Update form fields with the retrieved content
     submitButton.innerText = "Update";
-    holeForm.setAttribute("action", `edit/${holeId}`);
+    holeForm.setAttribute("action", `edit/${holeId}/`);
   });
 }
